b5d64b1eb9ee4582c53d40fb53cd440173d007b8
carloslago <carloslago@opendeusto.es>
2017-06-13 19:55:36 +0200

    Neo4j

--- app/src/main/java/com/example/android/movilidad_madrid/LoginActivity.java
+++ app/src/main/java/com/example/android/movilidad_madrid/LoginActivity.java
@@ -34,10 +34,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -202,7 +209,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             e.printStackTrace();
         }
         if (password_login.equals("")){
-
+            new PostDataTask().execute("http://192.168.56.1:1000/api/status");
 
             startActivity(new Intent(LoginActivity.this, LoginActivity.class));
         } else {
@@ -512,6 +519,85 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             } catch (IOException e){
                 return "Network error !";
+            }
+            return result.toString();
+        }
+    }
+
+    class PostDataTask extends AsyncTask<String, Void, String>{
+
+
+        ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(LoginActivity.this);
+            progressDialog.setMessage("Registrando usuario...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return postData(params[0]);
+            } catch (IOException e){
+                return "Network error!";
+            } catch (JSONException e){
+                return "Datos incorrectos";
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        }
+
+        private String postData(String urlPath) throws IOException, JSONException{
+
+            StringBuilder result = new StringBuilder();
+            BufferedWriter bufferedWriter = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                JSONObject dataToSend = new JSONObject();
+                dataToSend.put("username", UsernameView.getText().toString());
+                dataToSend.put("password", mPasswordView.getText().toString());
+
+                // Iniciar request
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json"); //header
+                urlConnection.connect();
+
+                // Escribir datos
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    result.append(line).append('\n');
+                }
+            } finally {
+                if (bufferedReader != null){
+                    bufferedReader.close();
+                }
+                if (bufferedWriter != null){
+                    bufferedWriter.close();
+                }
             }
             return result.toString();
         }
--- app/src/main/java/com/example/android/movilidad_madrid/MapsActivity.java
+++ app/src/main/java/com/example/android/movilidad_madrid/MapsActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.movilidad_madrid;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -29,19 +31,35 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.neo4j.driver.v1.*;
 import static org.neo4j.driver.v1.Values.parameters;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+//import org.neo4j.rest.graphdb.RestAPI;
+//import org.neo4j.rest.graphdb.RestAPIFacade;
+//import org.neo4j.rest.graphdb.query.QueryEngine;
+//import org.neo4j.rest.graphdb.query.RestCypherQueryEngine;
+//import org.neo4j.rest.graphdb.util.QueryResult;
 
 
 
@@ -52,18 +70,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private int PLACE_PICKER_REQUEST = 1;
     private PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+    private String Destino_Neo = "";
 
-//    Driver driver = GraphDatabase.driver("", AuthTokens.basic("neo4j", "neo4j"));
-//    Session session = driver.session();
-    //session.close();
-    //driver.close();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
         boolean hasLoggedIn = settings.getBoolean("hasLoggedIn", false);
-
+//
 //        Log.d("a", ""+hasLoggedIn);
 //        SharedPreferences.Editor editor = settings.edit();
 //        editor.putBoolean("hasLoggedIn", false);
@@ -95,7 +112,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 e.printStackTrace();
             }
         } else {
-            startActivity(new Intent(MapsActivity.this, MenuActivity.class));
+            startActivity(new Intent(MapsActivity.this, LoginActivity.class));
         }
 
 
@@ -137,29 +154,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.416733, -3.703313), 10));
     }
 
-//    public void onMapSearch(View view) {
-//        EditText locationSearch = (EditText) findViewById(R.id.editText);
-//        String location = locationSearch.getText().toString();
-//        System.out.print(location);
-//        List<android.location.Address> addressList = null;
-//        mMap.clear();
-//
-//        if (location != null || !location.equals("")) {
-//            Geocoder geocoder = new Geocoder(this);
-//            try {
-//                addressList = geocoder.getFromLocationName(location, 1);
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            android.location.Address address = addressList.get(0);
-//            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-//            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-//            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-//        }
-//    }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Driver driver = GraphDatabase.driver("bolt://hobby-ebblnhmhoeaggbkepcpfeipl.dbs.graphenedb.com:24786", AuthTokens.basic("root", "b.kw8DoVqt3lgi.wqQn17S5BBkUwGXA"));
+
+//        RestAPI graphDb = new RestAPIFacade("http://10.0.2.2:7474/db/data");
+//        QueryEngine engine =new RestCypherQueryEngine(graphDb);
+//        Session session = driver.session();
+//        engine.query("create (p:Person {name:\"Steve\"}) return p", Collections.EMPTY_MAP);
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
                 Place place = PlacePicker.getPlace(data, this);
@@ -167,12 +169,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
                 mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title((String) place.getName()));
+                Log.d("a", place.getLatLng().latitude + "," + place.getLatLng().longitude );
+                new JsonTask().execute("http://192.168.56.1:3000/?tagId=" + place.getLatLng().latitude + "," + place.getLatLng().longitude);
             }
         }
+
+//        session.close();
+//        driver.close();
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        ProgressDialog pd;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MapsActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder buffer = new StringBuilder();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            Destino_Neo = result;
+
+            String[] coordenadas = Destino_Neo.replaceAll("\\[", "").replaceAll("\\]", "").split(",");
+            float latitud = Float.parseFloat(coordenadas[0]);
+            float longitud = Float.parseFloat(coordenadas[1]);
+            mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitud, longitud))
+                    .title("Destino para aparcar"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitud, longitud), 17));
+        }
     }
 }
